43a755651d619e39b69456358a5145dc1243b92c
algebnaly <algebnaly@algebnaly.com>
2026-05-07 00:26:08 +0800

    feat: make ByteBuf and ByteArray more ergonomic

--- binserde/src/lib.rs
+++ binserde/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 
 pub use binserde_core::{
     Discriminant,
@@ -21,9 +21,21 @@ impl Encode for ByteBuf {
     }
 }
 
+impl From<Vec<u8>> for ByteBuf {
+    fn from(value: Vec<u8>) -> Self {
+        Self(value)
+    }
+}
+
 impl From<&[u8]> for ByteBuf {
     fn from(bytes: &[u8]) -> Self {
         Self(bytes.to_vec())
+    }
+}
+
+impl From<&str> for ByteBuf {
+    fn from(value: &str) -> Self {
+        Self::from(value.as_bytes())
     }
 }
 
@@ -33,16 +45,16 @@ impl<const N: usize> From<&[u8; N]> for ByteBuf {
     }
 }
 
-impl AsRef<[u8]> for ByteBuf {
-    fn as_ref(&self) -> &[u8] {
-        &self.0
+impl Deref for ByteBuf {
+    type Target = Vec<u8>;
+    fn deref(&self) -> &Self::Target {
+        self.0.as_ref()
     }
 }
 
-impl Deref for ByteBuf {
-    type Target = [u8];
-    fn deref(&self) -> &Self::Target {
-        self.as_ref()
+impl DerefMut for ByteBuf {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
 
@@ -62,6 +74,12 @@ impl<const N: usize> ByteArray<N> {
     }
 }
 
+impl<const N: usize> Default for ByteArray<N> {
+    fn default() -> Self {
+        Self::zero()
+    }
+}
+
 impl<const N: usize> Encode for ByteArray<N> {
     fn encode<E: Encoder>(&self, encoder: E) -> Result<(), E::Error> {
         encoder.encode_byte_array(&self.0)
@@ -72,6 +90,19 @@ impl<const N: usize> Decode for ByteArray<N> {
     fn decode<D: Decoder>(decoder: D) -> Result<Self, D::Error> {
         let v = decoder.decode_byte_array()?;
         Ok(Self(v))
+    }
+}
+
+impl<const N: usize> Deref for ByteArray<N> {
+    type Target = [u8; N];
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const N: usize> DerefMut for ByteArray<N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
 
